0055757559960286dbc5dfcbbab93147e61823a2
Anivarth <anivarthvarma@gmail.com>
2026-03-22 16:30:56 +0530

    iteration 13: fixed property.py by adding a docstring , reducing number of attributes by removing unused houses attribute and converting mortagage calculation into a helper function

--- moneypoly/moneypoly/moneypoly/property.py
+++ moneypoly/moneypoly/moneypoly/property.py
@@ -1,3 +1,5 @@
+"""Property and PropertyGroup classes for MoneyPoly board elements."""
+
 class Property:
     """Represents a single purchasable property tile on the MoneyPoly board."""
 
@@ -8,15 +10,18 @@ class Property:
         self.position = position
         self.price = price
         self.base_rent = base_rent
-        self.mortgage_value = price // 2
         self.owner = None
         self.is_mortgaged = False
-        self.houses = 0
 
         # Register with the group immediately on creation
         self.group = group
         if group is not None and self not in group.properties:
             group.properties.append(self)
+
+    @property
+    def mortgage_value(self):
+        """Return the mortgage value, defined as half the purchase price."""
+        return self.price // 2
 
     def get_rent(self):
         """
