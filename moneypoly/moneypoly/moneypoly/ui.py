e14baa56c1ba4e834db4994bfdb9c281cb42ad3c
Anivarth <anivarthvarma@gmail.com>
2026-03-22 16:23:56 +0530

    iteration 12: fixed ui.py by adding docstring, fixing try except syntax

--- moneypoly/moneypoly/moneypoly/ui.py
+++ moneypoly/moneypoly/moneypoly/ui.py
@@ -1,3 +1,4 @@
+"""User interface utilities for MoneyPoly."""
 def print_banner(title):
     """Print a decorated section header."""
     width = 52
@@ -66,7 +67,7 @@ def safe_int_input(prompt, default=0):
     """
     try:
         return int(input(prompt))
-    except:
+    except ValueError:
         return default
 
 
