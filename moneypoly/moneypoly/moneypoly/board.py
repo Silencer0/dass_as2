5cfcf3a6ff19afef7849806da5b807c5d4378f9c
Anivarth <anivarthvarma@gmail.com>
2026-03-23 16:34:28 +0530

    error 6: fixed the issue where railroads were missing from purchasable items

--- moneypoly/moneypoly/moneypoly/board.py
+++ moneypoly/moneypoly/moneypoly/board.py
@@ -48,6 +48,8 @@ class Board:
             "yellow":     PropertyGroup("Yellow",     "yellow"),
             "green":      PropertyGroup("Green",      "green"),
             "dark_blue":  PropertyGroup("Dark Blue",  "dark_blue"),
+            "railroad":   PropertyGroup("Railroad",   "black"),
+            "utility":    PropertyGroup("Utility",    "white"),
         }
 
     def _create_properties(self):
@@ -76,6 +78,12 @@ class Board:
             Property("Pennsylvania Avenue",    34, (320, 28), group=g["green"]),
             Property("Park Place",             37, (350, 35), group=g["dark_blue"]),
             Property("Boardwalk",              39, (400, 50), group=g["dark_blue"]),
+            Property("Reading Railroad",        5,  (200, 25), group=g["railroad"]),
+            Property("Pennsylvania Railroad",   15, (200, 25), group=g["railroad"]),
+            Property("B. & O. Railroad",        25, (200, 25), group=g["railroad"]),
+            Property("Short Line Railroad",     35, (200, 25), group=g["railroad"]),
+            Property("Electric Company",        12, (150, 4),  group=g["utility"]),
+            Property("Water Works",             28, (150, 4),  group=g["utility"]),
         ]
 
     def get_property_at(self, position):
