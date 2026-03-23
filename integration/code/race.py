2a6fa4f312089cf49896759d410d93264297e31a
Anivarth <anivarthvarma@gmail.com>
2026-03-24 00:22:33 +0530

    fix 1: the record race function inside race was supposed to retrieve a drivers skill level and increment it, but when a ghost driver is passed as the winner, it does None + 1 , causing an error

--- integration/code/race.py
+++ integration/code/race.py
@@ -4,5 +4,6 @@ class RaceManagement:
 
     def record_race(self, d1, c1, d2, c2, winner_name):
         curr_skill = self.crew.registration.get_skill(winner_name)
-        self.crew.registration.set_skill(winner_name, curr_skill + 1)
+        if curr_skill is not None:
+            self.crew.registration.set_skill(winner_name, curr_skill + 1)
         return winner_name
